0ce236bf9dd6a16d98d3178dbdf1ccaafd2029d7
Getachew Ganfur <getachewganfur@gmail.com>
2025-07-01 23:08:08 +0300

    feat: Pydantic models for API

---
+++ credit-risk-model/src/api/pydantic_models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class PredictionRequest(BaseModel):
+    # Define request fields here
+    pass
+
+class PredictionResponse(BaseModel):
+    # Define response fields here
+    pass 
